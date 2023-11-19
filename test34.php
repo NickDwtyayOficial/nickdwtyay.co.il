d4aba28e97e7fb40f40f39d52230a4e1dd0899c7
Nick Dwtyay Ltd <33708058+NickDwtyayOficial@users.noreply.github.com>
2023-11-19 13:16:54 +0200

    Create test34.php
    test34.php

---
+++ test34.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Notícias</title>
+</head>
+<body>
+    <h1>Últimas Notícias</h1>
+    <ul>
+        <li><a href="post.php?id=1">Título da Notícia 1</a></li>
+        <li><a href="post.php?id=2">Título da Notícia 2</a></li>
+        <li><a href="post.php?id=3">Título da Notícia 3</a></li>
+    </ul>
+</body>
+</html>
