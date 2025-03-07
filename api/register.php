7e88bd20b25d746bc3da81000fe8b9f163dafaf3
Nick Dwtyay Ltd <33708058+NickDwtyayOficial@users.noreply.github.com>
2025-03-07 04:18:14 +0300

    commit

--- api/register.php
+++ api/register.php
@@ -236,7 +236,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         "Americas and Middle East Cybersecurity Software and Technology Solutions Development Company."<br>
         <a href="https://www.nickdwtyay.com.br/Terms.html">Terms</a> |
         <a href="https://www.nickdwtyay.com.br/Privacy_Policy.html">Privacy Policy</a> |
-        All Rights Reserved | © 2006 - 2022 NICK DWTYAY
+        All Rights Reserved | © 2006 - 2025 NICK DWTYAY
     </footer>
 </body>
 </html>
