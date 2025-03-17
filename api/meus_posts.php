bd8081d0572990a89fc435b6d7bfbfa5193b18bf
Nick Dwtyay Ltd <33708058+NickDwtyayOficial@users.noreply.github.com>
2025-03-17 03:38:48 +0300

    commit

--- api/meus_posts.php
+++ api/meus_posts.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once __DIR__ . '/db_connect.php';
+require_once __DIR__ . '/db_connect.php'; nhl
 
 if (!isset($_SESSION['logado'])) {
     header("Location: login.php");
