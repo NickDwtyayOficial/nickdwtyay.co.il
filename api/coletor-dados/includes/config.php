70c6fd75f5c1bc957c17c7f29aff238532a3adef
Nick Dwtyay Ltd <33708058+NickDwtyayOficial@users.noreply.github.com>
2025-04-02 04:47:36 +0000

    commit

--- api/coletor-dados/includes/config.php
+++ api/coletor-dados/includes/config.php
@@ -1,1 +1,10 @@
+<?php
+// Configurações de segurança
+define('TOKEN_IPINFO', 'b9926cf26ae3ac'); // Substitua pelo seu token real
+define('LOG_FILE', __DIR__ . '/../storage/dados.csv');
 
+// Cria o arquivo de log se não existir
+if (!file_exists(LOG_FILE)) {
+    file_put_contents(LOG_FILE, "IP,Data,Hora,Cidade,Regiao,Pais,Provedor,LatLong\n");
+}
+?>
