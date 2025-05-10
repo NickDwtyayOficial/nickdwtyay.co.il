c7b47a6a73cb3c017c864fd6ab53884273407f08
Nick Dwtyay Ltd <33708058+NickDwtyayOficial@users.noreply.github.com>
2025-05-10 01:58:55 +0000

    commit

--- api/covid_report.php
+++ api/covid_report.php
@@ -110,12 +110,6 @@ $pdf_path = '/api/covid_report.pdf'; // Caminho relativo na raiz
         <p>Por Nicássio Guimarães</p>
         <embed src="<?php echo htmlspecialchars($pdf_path); ?>" type="application/pdf">
     </div>
-    <footer class="footer">
-        NICK DWTYAY, LTD.<br>
-        "Americas and Middle East Cybersecurity Software and Technology Solutions Development Company."<br>
-        <a href="/Terms.php">Terms</a> |
-        <a href="/Privacy_Policy.php">Privacy Policy</a> |
-        All Rights Reserved | © 2006 - 2025 Nick Dwtyay, Ltd.
-    </footer>
+    <?php include __DIR__ . '/includes/footer.php'; ?>
 </body>
 </html>
