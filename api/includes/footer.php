170c64d9b9049fa6853cc8a5382fcebaf1fc1700
Nick Dwtyay Ltd <33708058+NickDwtyayOficial@users.noreply.github.com>
2025-05-10 14:26:14 +0000

    commit

--- api/includes/footer.php
+++ api/includes/footer.php
@@ -8,13 +8,12 @@
     <a href="/Privacy_Policy.php">Privacy Policy</a> |
     All Rights Reserved © 2006 - 2025 Nick Dwtyay, Ltd.
 </footer>
+
 <script>
     window.va = window.va || function (...args) { (window.vaq = window.vaq || []).push(args); };
 </script>
 <script src="/_vercel/insights/script.js" defer></script>
-<script src="https://cdn.jsdelivr.net/npm/@statsig/js-client@3/build/statsig-js-client+session-replay+web-analytics.min.js?apikey=client-WA7hJS8vXPG4k3WEXJ6V4raziKM5xH4WFc08sYT38qQ">
-</script>
-
+</body>
 
 </body>
 </html>
