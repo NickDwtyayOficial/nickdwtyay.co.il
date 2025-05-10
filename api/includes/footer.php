65b8dc4e57d4866d7151ac416a621c0ab9d0eaed
Nick Dwtyay Ltd <33708058+NickDwtyayOficial@users.noreply.github.com>
2025-05-10 01:52:14 +0000

    commit

---
+++ api/includes/footer.php
@@ -0,0 +1,16 @@
+<?php
+// api/includes/footer.php
+?>
+<footer class="footer">
+    NICK DWTYAY, LTD.<br>
+    "Americas and Middle East Cybersecurity Software and Technology Solutions Development Company."<br>
+    <a href="/Terms.php">Terms</a> |
+    <a href="/Privacy_Policy.php">Privacy Policy</a> |
+    All Rights Reserved © 2006 - 2025 Nick Dwtyay, Ltd.
+</footer>
+<script>
+    window.va = window.va || function (...args) { (window.vaq = window.vaq || []).push(args); };
+</script>
+<script src="/_vercel/insights/script.js" defer></script>
+</body>
+</html>
