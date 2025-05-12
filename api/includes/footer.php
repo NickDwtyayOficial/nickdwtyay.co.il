8094e99276dfe5b42c5f8056003e3c95e8777858
Nick Dwtyay Ltd <33708058+NickDwtyayOficial@users.noreply.github.com>
2025-05-12 00:04:04 +0000

    commit

--- api/includes/footer.php
+++ api/includes/footer.php
@@ -13,7 +13,20 @@
     window.va = window.va || function (...args) { (window.vaq = window.vaq || []).push(args); };
 </script>
 <script src="/_vercel/insights/script.js" defer></script>
-</body>
 
+
+<!-- Default Statcounter code for login http://nickdwtyay.com.br -->
+<script type="text/javascript">
+var sc_project=13135134; 
+var sc_invisible=1; 
+var sc_security="9cbf285a"; 
+</script>
+<script type="text/javascript"
+src="https://www.statcounter.com/counter/counter.js" async></script>
+<noscript><div class="statcounter"><a title="Web Analytics"
+href="https://statcounter.com/" target="_blank"><img class="statcounter"
+src="https://c.statcounter.com/13135134/0/9cbf285a/1/" alt="Web Analytics"
+referrerPolicy="no-referrer-when-downgrade"></a></div></noscript>
+<!-- End of Statcounter Code -->
 </body>
 </html>
