b36ef140866aae67db46e493d6b550c34d9f0420
Nick Dwtyay Ltd <33708058+NickDwtyayOficial@users.noreply.github.com>
2025-05-14 04:36:26 +0000

    commit
    só falta o CSS <!-- api/includes/head.php -->
    <head>
        <meta charset="UTF-8">
        <meta name="viewport" content="width=device-width, initial-scale=1.0">
        <title><?php echo isset($pageTitle) ? htmlspecialchars($pageTitle) : "Nick Dwtyay - Home"; ?></title>
        <meta name="description" content="<?php echo isset($pageDescription) 
            ? htmlspecialchars($pageDescription) 
            : 'Nick Dwtyay Ltd is a Middle East-based Telecommunications, Cybersecurity Software and Technology Solution Development Company offering innovative solutions to businesses and individuals. Our partnerships in South America give us the opportunity to provide world-class services to our customers.'; ?>" />
        <link rel="icon" type="image/gif" href="/api/static/dwtyay_favicon.gif">
        <!-- Outros links e scripts -->
    </head>

--- api/includes/head.php
+++ api/includes/head.php
@@ -2,9 +2,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo isset($pageTitle) ? htmlspecialchars($pageTitle) : "Login Nick Dwtyay, Ltd. "; ?></title>
-    <meta name="description" content="<?php echo isset($pageDescription) ? htmlspecialchars($pageDescription) : 'Loja, tecnologia e segurança Nick Dwtyay Ltd.'; ?>">
-    <meta name="keywords" content="loja, segurança, tecnologia, Nick Dwtyay, software, cybersecurity">
+    <title><?php echo isset($pageTitle) ? htmlspecialchars($pageTitle) : "Nick Dwtyay - Home"; ?></title>
+    <meta name="description" content="<?php echo isset($pageDescription) 
+        ? htmlspecialchars($pageDescription) 
+        : 'Nick Dwtyay Ltd is a Middle East-based Telecommunications, Cybersecurity Software and Technology Solution Development Company offering innovative solutions to businesses and individuals. Our partnerships in South America give us the opportunity to provide world-class services to our customers.'; ?>" />
     <link rel="icon" type="image/gif" href="/api/static/dwtyay_favicon.gif">
-    <!-- Outros links/scripts -->
+    <!-- Outros links e scripts -->
 </head>
