d03b0364b8bde590f6100f9b7e0a2701a16f92b6
Nick Dwtyay Ltd <33708058+NickDwtyayOficial@users.noreply.github.com>
2025-05-14 04:30:26 +0000

    commit
     fazendo alterações gradual

--- api/includes/head.php
+++ api/includes/head.php
@@ -1,1 +1,10 @@
-
+<!-- api/includes/head.php -->
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo isset($pageTitle) ? htmlspecialchars($pageTitle) : "Login Nick Dwtyay, Ltd. "; ?></title>
+    <meta name="description" content="<?php echo isset($pageDescription) ? htmlspecialchars($pageDescription) : 'Loja, tecnologia e segurança Nick Dwtyay Ltd.'; ?>">
+    <meta name="keywords" content="loja, segurança, tecnologia, Nick Dwtyay, software, cybersecurity">
+    <link rel="icon" type="image/gif" href="/api/static/dwtyay_favicon.gif">
+    <!-- Outros links/scripts -->
+</head>
