37f64d93a6cf7f099cf7aa49ea868b16524f1bef
Nick Dwtyay Ltd <33708058+NickDwtyayOficial@users.noreply.github.com>
2025-05-16 21:50:02 +0000

    commit

--- api/404.php
+++ api/404.php
@@ -1,8 +1,6 @@
 
 <?php
 http_response_code(404);
-?>
-
 <?php
 $pageTitle = "404 erro";
 $pageDescription = "Nick Dwtyay Ltd, is a Middle East-based Telecommunications, Cybersecurity Software and Technology Solution Development Company offering innovative solutions to businesses and individuals. Our partnerships in South America give us the opportunity to provide world-class services to our customers. essa página possivelmente está em manutenção e por isso você está visualizando ela";
