b2918a6183631cf9b956e47c30904d4bf448cee9
Nick Dwtyay Ltd <33708058+NickDwtyayOficial@users.noreply.github.com>
2025-05-16 22:53:37 +0000

    commmit

--- api/JavaScript/statcounter.js
+++ api/JavaScript/statcounter.js
@@ -1,1 +1,19 @@
 
+
+<!-- Default Statcounter code for login
+http://nickdwtyay.com.br -->
+<script type="text/javascript">
+var sc_project=13135134; 
+var sc_invisible=1; 
+var sc_security="9cbf285a"; 
+</script>
+<script type="text/javascript"
+src="https://www.statcounter.com/counter/counter.js"
+async></script>
+<noscript><div class="statcounter"><a title="Web Analytics
+Made Easy - Statcounter" href="https://statcounter.com/"
+target="_blank"><img class="statcounter"
+src="https://c.statcounter.com/13135134/0/9cbf285a/1/"
+alt="Web Analytics Made Easy - Statcounter"
+referrerPolicy="no-referrer-when-downgrade"></a></div></noscript>
+<!-- End of Statcounter Code -->
