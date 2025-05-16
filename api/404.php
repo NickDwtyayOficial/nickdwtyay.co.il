d43da4109607b7169e7b08128cf5c6cdfb65959e
Nick Dwtyay Ltd <33708058+NickDwtyayOficial@users.noreply.github.com>
2025-05-16 22:51:36 +0000

    commit

--- api/404.php
+++ api/404.php
@@ -1,10 +1,13 @@
 <?php
 http_response_code(404);
 $pageTitle = "404 erro";
-$pageDescription = "Nick Dwtyay Ltd, is a Middle East-based Telecommunications, Cybersecurity Software and Technology Solution Development Company offering innovative solutions to businesses and individuals. Our partnerships in South America give us the opportunity to provide world-class services to our customers. essa página possivelmente está em manutenção e por isso você está visualizando ela";
-include __DIR__ . '/includes/head.php';
+$pageDescription = "Nick Dwtyay Ltd, is a Middle East-based Telecommunications,
+Cybersecurity Software and Technology Solution Development Company offering innovative solutions to businesses and individuals.
+Our partnerships in South America give us the opportunity to provide world-class services to our customers.
+essa página possivelmente está em manutenção e por isso você está visualizando ela";
 ?><!DOCTYPE html>
 <html lang="pt-BR">
+<?php include __DIR__ . '/includes/head.php'; ?>
 <body>
     <h1>Erro 404</h1>
     <p>Ops! A página que você procura não foi encontrada.</p>
