534e9b912d526754a0c735e3b5684ea51b5e058d
Nick Dwtyay Ltd <33708058+NickDwtyayOficial@users.noreply.github.com>
2025-05-20 20:26:48 +0000

    commit

--- api/includes/footer.php
+++ api/includes/footer.php
@@ -1,56 +1,36 @@
 <?php
 // api/includes/footer.php
 ?>
-<footer class="footer">
-    <footer style="background:#222; color:#fff; padding:20px 0; text-align:center;">
-<div style="margin: 20px 0;">
-  <span>Siga-nos:</span>
-  <a href="https://facebook.com/nikc.dwtyay" target="_blank" style="margin:0 10px; color:#3b5998;" title="Facebook">
-    <i class="fab fa-facebook-f"></i>
-  </a>
-  <a href="https://www.kwai.com/@NICK_DWTYAY" target="_blank" style="margin:0 10px;" title="Kwai">
-    <img src="https://cdn-static.kwai.net/kos/s101/nlav11312/icon/kwai/2/favicon.ico" alt="Kwai" style="width:18px;vertical-align:middle;">
-  </a>
-  <a href="https://instagram.com/nickdwtyay" target="_blank" style="margin:0 10px; color:#E1306C;" title="Instagram">
-    <i class="fab fa-instagram"></i>
-  </a>
-  <a href="https://il.linkedin.com/in/nic%C3%A1ssio-guimar%C3%A3es-b0660223b" target="_blank" style="margin:0 10px; color:#0077b5;" title="LinkedIn">
-    <i class="fab fa-linkedin-in"></i>
-  </a>
-  <a href="https://www.tiktok.com/@nick.dwtyay" target="_blank" style="margin:0 10px; color:#010101;" title="TikTok">
-    <i class="fab fa-tiktok"></i>
-  </a>
-  <a href="https://x.com/dwtyayp" target="_blank" style="margin:0 10px; color:#000;" title="X (Twitter)">
-    <i class="fab fa-x-twitter"></i>
-  </a>
-</div>
-   
+<footer class="footer" style="background:#222; color:#fff; padding:20px 0; text-align:center;">
+    <div class="footer-social" style="margin: 20px 0;">
+        <span>Siga-nos:</span>
+        <a href="https://facebook.com/nikc.dwtyay" target="_blank" style="margin:0 10px; color:#3b5998;" title="Facebook">
+            <i class="fab fa-facebook-f"></i>
+        </a>
+        <a href="https://www.kwai.com/@NICK_DWTYAY" target="_blank" style="margin:0 10px;" title="Kwai">
+            <img src="https://cdn-static.kwai.net/kos/s101/nlav11312/icon/kwai/2/favicon.ico" alt="Kwai" style="width:18px;vertical-align:middle;">
+        </a>
+        <a href="https://instagram.com/nickdwtyay" target="_blank" style="margin:0 10px; color:#E1306C;" title="Instagram">
+            <i class="fab fa-instagram"></i>
+        </a>
+        <a href="https://il.linkedin.com/in/nic%C3%A1ssio-guimar%C3%A3es-b0660223b" target="_blank" style="margin:0 10px; color:#0077b5;" title="LinkedIn">
+            <i class="fab fa-linkedin-in"></i>
+        </a>
+        <a href="https://www.tiktok.com/@nick.dwtyay" target="_blank" style="margin:0 10px; color:#010101;" title="TikTok">
+            <i class="fab fa-tiktok"></i>
+        </a>
+        <a href="https://x.com/dwtyayp" target="_blank" style="margin:0 10px; color:#000;" title="X (Twitter)">
+            <i class="fab fa-x-twitter"></i>
+        </a>
+    </div>
     NICK DWTYAY, LTD.<br>
     "Americas and Middle East Cybersecurity Software and Technology Solutions Development Company."<br>
     <a href="/Terms.php">Terms</a> |
     <a href="/Privacy_Policy.php">Privacy Policy</a> |
     All Rights Reserved © 2006 - 2025 Nick Dwtyay, Ltd.
 </footer>
-
+<!-- Scripts extras (se realmente forem necessários para todas as páginas) -->
 <script>
     window.va = window.va || function (...args) { (window.vaq = window.vaq || []).push(args); };
 </script>
 <script src="/_vercel/insights/script.js" defer></script>
-
-<script src="/api/public/statcounter.js"></script>
-
-<!-- Default Statcounter code for login http://nickdwtyay.com.br -->
-<script type="text/javascript">
-var sc_project=13135134; 
-var sc_invisible=1; 
-var sc_security="9cbf285a"; 
-</script>
-<script type="text/javascript"
-src="https://www.statcounter.com/counter/counter.js" async></script>
-<noscript><div class="statcounter"><a title="Web Analytics"
-href="https://statcounter.com/" target="_blank"><img class="statcounter"
-src="https://c.statcounter.com/13135134/0/9cbf285a/1/" alt="Web Analytics"
-referrerPolicy="no-referrer-when-downgrade"></a></div></noscript>
-<!-- End of Statcounter Code -->
-</body>
-</html>
