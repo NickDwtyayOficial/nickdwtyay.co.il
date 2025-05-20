105c36f2c7081775b1c3229e0a026f44581250cb
Nick Dwtyay Ltd <33708058+NickDwtyayOficial@users.noreply.github.com>
2025-05-20 20:56:35 +0000

    commit

--- api/includes/head.php
+++ api/includes/head.php
@@ -5,14 +5,15 @@
     <meta name="description" content="<?php echo isset($pageDescription) 
         ? htmlspecialchars($pageDescription) 
         : 'Nick Dwtyay Ltd is a Middle East-based Telecommunications, Cybersecurity Software and Technology Solution Development Company offering innovative solutions to businesses and individuals. Our partnerships in South America give us the opportunity to provide world-class services to our customers.'; ?>" />
-    <meta http-equiv="Content-Security-Policy" content="default-src 'self'; img-src 'self' data:; media-src 'self' data:; script-src 'self'
- https://cdnjs.cloudflare.com https://static.cloudflareinsights.com https://pagead2.googlesyndication.com https://www.googletagmanager.com
-        https://www.statcounter.com 'unsafe-inline'; connect-src 'self' 
-        https://*.vercel.app https://seu-projeto.supabase.co
-        https://cloudflareinsights.com https://ipinfo.io
-        https://ipqualityscore.com https://proxycheck.io https://www.google-analytics.com https://stats.g.doubleclick.net
-        https://c.statcounter.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://fonts.gstatic.com; img-src 'self'
-        https://c.statcounter.com; font-src 'self' https://fonts.gstatic.com;">
+   <meta http-equiv="Content-Security-Policy"
+      content="
+        default-src 'self';
+        style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdnjs.cloudflare.com;
+        font-src 'self' https://fonts.gstatic.com https://cdnjs.cloudflare.com;
+        script-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com https://static.cloudflareinsights.com https://pagead2.googlesyndication.com https://www.googletagmanager.com https://www.statcounter.com;
+        img-src 'self' data: https://c.statcounter.com https://cdn-static.kwai.net;
+        connect-src 'self' https://*.vercel.app https://zeqmcwtwhjzqhcvfsvli.supabase.co https://cloudflareinsights.com https://ipinfo.io https://ipqualityscore.com https://proxycheck.io https://www.google-analytics.com https://stats.g.doubleclick.net https://c.statcounter.com;
+      ">
     <?php if (isset($pageExtraMeta)) echo $pageExtraMeta; ?>
     <link rel="icon" type="image/gif" href="/api/static/dwtyay_favicon.gif">
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap" rel="stylesheet">
