9d635415e94e99816f34f0ef5545845ad4ba2720
Nick Dwtyay Ltd <33708058+NickDwtyayOficial@users.noreply.github.com>
2025-05-20 20:01:46 +0000

    commit

--- api/includes/footer.php
+++ api/includes/footer.php
@@ -2,6 +2,29 @@
 // api/includes/footer.php
 ?>
 <footer class="footer">
+    <footer style="background:#222; color:#fff; padding:20px 0; text-align:center;">
+<div style="margin: 20px 0;">
+  <span>Siga-nos:</span>
+  <a href="https://facebook.com/nikc.dwtyay" target="_blank" style="margin:0 10px; color:#3b5998;" title="Facebook">
+    <i class="fab fa-facebook-f"></i>
+  </a>
+  <a href="https://www.kwai.com/@NICK_DWTYAY" target="_blank" style="margin:0 10px;" title="Kwai">
+    <img src="https://cdn-static.kwai.net/kos/s101/nlav11312/icon/kwai/2/favicon.ico" alt="Kwai" style="width:18px;vertical-align:middle;">
+  </a>
+  <a href="https://instagram.com/nickdwtyay" target="_blank" style="margin:0 10px; color:#E1306C;" title="Instagram">
+    <i class="fab fa-instagram"></i>
+  </a>
+  <a href="https://il.linkedin.com/in/nic%C3%A1ssio-guimar%C3%A3es-b0660223b" target="_blank" style="margin:0 10px; color:#0077b5;" title="LinkedIn">
+    <i class="fab fa-linkedin-in"></i>
+  </a>
+  <a href="https://www.tiktok.com/@nick.dwtyay" target="_blank" style="margin:0 10px; color:#010101;" title="TikTok">
+    <i class="fab fa-tiktok"></i>
+  </a>
+  <a href="https://x.com/dwtyayp" target="_blank" style="margin:0 10px; color:#000;" title="X (Twitter)">
+    <i class="fab fa-x-twitter"></i>
+  </a>
+</div>
+   
     NICK DWTYAY, LTD.<br>
     "Americas and Middle East Cybersecurity Software and Technology Solutions Development Company."<br>
     <a href="/Terms.php">Terms</a> |
